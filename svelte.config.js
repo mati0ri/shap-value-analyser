e0321fb121e075a822214af9d9520d5bfc708c00
mati0ri <mateo.iori223@gmail.com>
2026-02-23 10:27:47 +0100

    add gitlab-ci

--- svelte.config.js
+++ svelte.config.js
@@ -4,13 +4,7 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
+			fallback: '200.html'
 		})
 	}
 };
